379d4d3b4850d38a67141872fa3e25e6707dc8ac
Kedar Shinde <kedarshinde8011@gmail.com>
2023-09-25 23:27:43 +0530

    add draw lines

--- go.mod
+++ go.mod
@@ -4,11 +4,12 @@ go 1.20
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell v1.4.0 // indirect
 	github.com/gdamore/tcell/v2 v2.6.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.14 // indirect
-	github.com/rivo/uniseg v0.4.3 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/term v0.12.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
 import (
-    "github.com/gdamore/tcell/v2"
-    "os"
+	"os"
+	"strconv"
+
+	"github.com/gdamore/tcell/v2"
 )
+
+type Config struct {
+    rows int
+    cols int
+}
+
+var config Config
+
+func drawRows()  {
+    for i:=0 ; i < config.rows; i++ {
+        s := strconv.Itoa(i)
+        os.Stdout.WriteString(s+"\r\n")
+    }
+}
+
+func refreshScreen()  {
+    os.Stdout.WriteString("\x1b[H")
+
+    drawRows()
+
+    os.Stdout.WriteString("\x1b[H")
+}
+
+func readKey(ev *tcell.EventKey)  {
+    key := ev.Key()
+    if key == tcell.KeyCtrlQ {
+        os.Exit(0)
+    }
+}
+
+func handleEvents(screen tcell.Screen)  {
+    ev := screen.PollEvent()
+    switch ev:= ev.(type) {
+
+
+    case *tcell.EventKey:
+        readKey(ev)
+    }
+}
+
+func configUpdate(screen tcell.Screen)  {
+    config.rows, config.cols = screen.Size()
+}
 
 func main() {
     // Create a new screen object
@@ -18,15 +63,13 @@ func main() {
     }
     defer screen.Fini()
 
+
+
     // Wait for events
     for {
-        ev := screen.PollEvent()
-        switch ev := ev.(type) {
-        case *tcell.EventKey:
-            key := ev.Key()
-			if key == tcell.KeyCtrlC {
-				os.Exit(0)
-			}
-        }
+        configUpdate(screen)
+        screen.Clear()
+        refreshScreen()
+        handleEvents(screen)
     }
 }
